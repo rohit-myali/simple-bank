63940e9f59bffc0b0f7076d3ef0e8c80d29d7256
Rohit Myali <rohit.myali@citrix.com>
2025-07-06 01:18:55 +0530

    fix: sync generated Go code with SQL query changes
    Regenerate sqlc code after previously removing AddAccountBalance query from account.sql
    
    Signed-off-by: Rohit Myali <rohit.myali@citrix.com>

--- db/sqlc/account.sql.go
+++ db/sqlc/account.sql.go
@@ -9,31 +9,6 @@ import (
 	"context"
 )
 
-const addAccountBalance = `-- name: AddAccountBalance :one
-UPDATE accounts
-SET balance = balance + $2
-WHERE id = $1
-RETURNING id, owner, balance, currency, created_at
-`
-
-type AddAccountBalanceParams struct {
-	ID      int64 `json:"id"`
-	Balance int64 `json:"balance"`
-}
-
-func (q *Queries) AddAccountBalance(ctx context.Context, arg AddAccountBalanceParams) (Account, error) {
-	row := q.db.QueryRowContext(ctx, addAccountBalance, arg.ID, arg.Balance)
-	var i Account
-	err := row.Scan(
-		&i.ID,
-		&i.Owner,
-		&i.Balance,
-		&i.Currency,
-		&i.CreatedAt,
-	)
-	return i, err
-}
-
 const createAccount = `-- name: CreateAccount :one
 INSERT INTO accounts (
   owner,
